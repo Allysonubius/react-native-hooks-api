a7b4cf5819201aa78ac8ebd8db710866861d4ab6
allyson <allyson.brandao@venhapranuvem.com.br>
2021-02-21 23:05:16 -0300

    Navegação Stack #02

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigation } from '@react-navigation/stack'
 import UserList from './views/UserList'
 import UserForm from './views/UserForm'
+import { Button , Icon } from 'react-native-elements'
 
 const Stack = createStackNavigation()
 
@@ -11,18 +12,44 @@ export default props =>{
         <NavigationContainer>
             <Stack.Navigator
                 initialRouteName="UserList"
+                screenOptions={screenOptions}
             >
                 <Stack.Screen
                     name="UserList"
-                    component={UserList}    
+                    component={UserList}
+                    options={({ navigation }) =>{
+                        return{
+                            title: "Lista de usuários",
+                            headerRight : () =>(
+                                <Button
+                                    onPress={() => navigation.navigate("UserForm")}
+                                    type="clear"
+                                    icon={<Icon name="add" size={25} color="white"/>}
+                                />
+                            )
+                        }
+                    }}
                 >
                 </Stack.Screen>
                 <Stack.Screen
                     name="UserList"
-                    component={UserForm}    
+                    component={UserForm}
+                    options={{
+                        title:"Formulário de Usuários"
+                    }}
                 >
                 </Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
     )
+}
+
+const screenOptions={
+    headerStyle:{
+        backgroundColor:'#f4511e'
+    },
+    headerTintColor:'#fff',
+    headerTitleColor:{
+        fontWeight:'bold'
+    }
 }
