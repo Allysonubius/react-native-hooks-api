30735b48f66d394b637df4d0a9fb68336a771148
allyson <allyson.brandao@venhapranuvem.com.br>
2021-02-21 23:11:30 -0300

    Criado arquivo JSON para interpretar lista

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id:1,
+        name: ' João Silva',
+        email: 'shaushausaush@gmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/02/19/11/37/lens-1209823_960_720.jpg'
+    },
+    {
+        id:2,
+        name: ' Mariana Aguiar',
+        email: 'shaushausaush@gmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/12/23/12/40/night-1927265_960_720.jpg'
+    },
+    {
+        id:3,
+        name: ' Julio Fragoso',
+        email: 'shaushausaush@gmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/08/26/01/32/poseidon-1621062_960_720.jpg'
+    },
+    {
+        id:4,
+        name: ' Rafael Monteiro',
+        email: 'shaushausaush@gmail.com',
+        avatarUrl:
+            'https://cdn.pixabay.com/photo/2016/11/10/07/50/glass-ball-1813707_960_720.jpg'
+    }
+]
