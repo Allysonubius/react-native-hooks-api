e696235f5dfa2203b481de3f5558e444f6deb049
allyson <allyson.brandao@venhapranuvem.com.br>
2021-02-21 23:35:28 -0300

    Lista de usuario #02

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,9 +1,40 @@
 import React from 'react'
-import { FlatList, Text } from 'react-native'
-import { ListItem } from 'react-native-elements'
+import { FlatList} from 'react-native'
+import { ListItem , Icon} from 'react-native-elements'
 import users from '../data/users'
 
 export default props =>{
+    
+    function confirmUserDelete(user){
+        Alert.alert('Excluir usuário', 'Deseja excluir o usuário ?',[
+            {
+                text:'SIM',
+                onPress=() => {
+                    console.warn('delete' + user.name)
+                }
+            },
+            {
+                text:'NÂO'
+            },
+        ])
+    }
+
+    function getActions(user){
+        return(
+            <>
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type="clear"
+                    icons={<Icon name="edit" size={25} color="orange"/>}
+                />
+                <Button
+                    onPress={() => confirmUserDelete(user)}
+                    type="clear"
+                    icons={<Icon name="delete" size={25} color="red"/>}
+                />
+            </>
+        )
+    }
 
     function getUserItem({ item: user }){
         return (
@@ -13,6 +44,7 @@ export default props =>{
                 title={user.name}
                 subtitle={user.email}
                 bottomDivider={true}
+                rightElement={getActions(user)}
                 onPress={() =>
                     props.navigation.navigate('UserForm')
                 }
