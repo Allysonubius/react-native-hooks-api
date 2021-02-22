20fdd5f4cae65ccf583c88dd906c77e7a353ff5d
allyson <allyson.brandao@venhapranuvem.com.br>
2021-02-21 23:20:52 -0300

    Lista de usuários #02

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,33 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { FlatList, Text } from 'react-native'
+import { ListItem } from 'react-native-elements'
+import users from '../data/users'
 
 export default props =>{
+
+    function getUserItem({ item: user }){
+        return (
+            <ListItem 
+                leftAvatar={{source: {uri: user.avatarUrl}}}
+                key={user.id}
+                title={user.name}
+                subtitle={user.email}
+                bottomDivider={true}
+                onPress={() =>
+                    props.navigation.navigate('UserForm')
+                }
+            />
+        )
+    }
+
     return (
-        <Text>User List</Text>
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            >
+            </FlatList>
+        </View>
     )
 }
